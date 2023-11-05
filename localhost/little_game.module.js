4fe912f04e7208f203eeddf9b590582419eaa7c5
legion5pro <you@example.com>
2023-11-05 18:18:43 +0100

    game

--- localhost/little_game.module.js
+++ localhost/little_game.module.js
@@ -1,10 +1,10 @@
 import {
-    f_o_vec2,
-    f_o_vec3, 
-    f_o_vec4
+    O_vec2,
+    O_vec3,
+    O_vec4,
 } from "./module.js"
-let n_tau = (Math.PI*2)
 
+let n_tau = (Math.PI*2)
 
 function distanceBetweenPoints(x1, y1, x2, y2) {
     let dx = x2 - x1;
@@ -16,44 +16,71 @@ function distanceBetweenPoints(x1, y1, x2, y2) {
     return distanceBetweenPoints(x, y, circle.centerX, circle.centerY) < circle.radius;
   }
   
-  function f_b_collision__circle_and_polygonpoints(circle, polygonVertices) {
-    let collision = false;
-  
-    // Check distance from circle center to polygon edges
-    for (let i = 0; i < polygonVertices.length; i++) {
-      let nextIndex = (i + 1) % polygonVertices.length;
-      let edgeStart = polygonVertices[i];
-      let edgeEnd = polygonVertices[nextIndex];
-  
-      let edgeLength = distanceBetweenPoints(edgeStart.x, edgeStart.y, edgeEnd.x, edgeEnd.y);
-      let distanceToCircleCenter = Math.abs((edgeEnd.x - edgeStart.x) * (edgeStart.y - circle.centerY) - 
-                                            (edgeStart.x - circle.centerX) * (edgeEnd.y - edgeStart.y)) / edgeLength;
-  
-      if (distanceToCircleCenter < circle.radius) {
-        collision = true; // The circle and polygon might be colliding
-        break;
-      }
+  let f_b_collision_square = function(
+    o_vec2_min_1,
+    o_vec2_max_1,
+    o_vec2_min_2,
+    o_vec2_max_2,
+  ){
+    return ((
+        o_vec2_min_1.n_x > o_vec2_min_2.n_x 
+        &&
+        o_vec2_min_1.n_x < o_vec2_max_2.n_x 
+    )
+    && 
+    (
+        o_vec2_max_1.n_x < o_vec2_max_2.n_x 
+        &&
+        o_vec2_max_1.n_x > o_vec2_min_2.n_x 
+    ))
+  }
+  function f_b_collision__circle_and_polygonpoints(
+    a_o_trn__shape_point, 
+    o_vec2_min_polygon,
+    o_vec2_max_polygon,
+    n_radius, 
+    o_trn_circle
+  ) {
+    let o_vec2_min_circle = o_trn_circle.sub(n_radius);
+    let o_vec2_max_circle = o_trn_circle.add(n_radius);
+
+    let b_collision_square = f_b_collision_square(
+        o_vec2_min_circle, 
+        o_vec2_max_circle,
+        o_vec2_min_polygon,
+        o_vec2_max_polygon,
+    )
+    if(b_collision_square){debugger}
+    if(
+        !b_collision_square
+    ){
+        // if the bounding boxes do not collide, there is no reason to go further with calculations
+        return false;
     }
-  
-    // Check if any vertex of polygon is inside the circle
-    for (let vertex of polygonVertices) {
-      if (isPointInsideCircle(vertex.x, vertex.y, circle)) {
-        collision = true;
-        break;
-      }
+    let n_intersections = 0;
+    // Loop through each edge of the polygon
+    for (let n_i = 0; n_i < a_o_trn__shape_point.length; n_i++) {
+        let o_trn__shape_point = a_o_trn__shape_point[n_i]
+        let o_trn__shape_point_next = a_o_trn__shape_point[(n_i + 1) % a_o_trn__shape_point.length]
+
+        // Check if the line intersects with the horizontal ray cast from the circle center
+        if ((o_trn__shape_point.n_y > o_trn_circle.n_x) != (o_trn__shape_point_next.n_y > o_trn_circle.n_y)) { // If circleCenter.y lies between yi and yj
+            let xIntersection = 
+                (o_trn__shape_point.n_y - o_trn__shape_point.n_x)
+                    * (o_trn_circle.n_y - o_trn__shape_point.n_y) 
+                    / (o_trn__shape_point_next.n_y - o_trn__shape_point.n_y)
+                    + o_trn__shape_point.n_x;
+
+            if (o_trn_circle.n_x < xIntersection) { // If the intersection is to the right of the circle center
+                n_intersections++;
+            }
+        }
     }
-  
-    // If no collision has been detected so far, check if the circle is inside the polygon
-    if (!collision) {
-      // Take a point on the circle (e.g., to the right of the center)
-      let pointOnCircle = {
-        x: circle.centerX + circle.radius * Math.cos(0), // 0 radians (to the right)
-        y: circle.centerY + circle.radius * Math.sin(0)
-      };
-      collision = !isPointInsidePolygon(pointOnCircle.x, pointOnCircle.y, polygonVertices);
-    }
-  
-    return collision;
+        
+        // Odd number of intersections means the point is inside the polygon
+        return n_intersections % 2 !== 0;
+        
+    return false
   }
   
   function isPointInsidePolygon(x, y, polygonVertices) {
@@ -154,7 +181,7 @@ window.o_state = null;
 let o_go__bad_meteroite = null;
 let o_go__ship = new O_go(
     function(){
-        this.o_trn = f_o_vec2(20)
+        this.o_trn = new O_vec2(20)
         this.n_radius = 50
         this.n_rot_rad = 0
         this.n_corners = 3
@@ -165,14 +192,14 @@ let o_go__ship = new O_go(
     [],
     'rgba(255,255,255,1)',
     function(){
-        let o_diff = f_o_vec2(o_state.o_trn__mouse).subtract(this.o_trn);
+        let o_diff = new O_vec2(o_state.o_trn__mouse).subtract(this.o_trn);
         let n_dist = o_diff.length();
         let n_ang_rad = this.o_trn.hangle(o_state.o_trn__mouse)
         // this.s_color = `rgba(222,222,222,${(Math.sin(o_state.n_t)*.5+0.5)}`
         this.s_color = `rgba(222,222,222,1)`
-        let o_trn_speed = f_o_vec2(
-            Math.cos(n_ang_rad+(n_tau/4))*2,
-            Math.sin(n_ang_rad+(n_tau/4))*2,
+        let o_trn_speed = new O_vec2(
+            Math.sin(n_ang_rad+(n_tau/2))*2,
+            Math.cos(n_ang_rad+(n_tau/2))*2,
         )//base speed
         if(o_state.b_mousedown_r){
             
@@ -199,13 +226,11 @@ let o_go__ship = new O_go(
 
                 let o_go__shot = new O_go(
                     function(){
-                        this.o_trn = f_o_vec2(o_self.o_trn)
-                        this.n_radius = 2
+                        this.o_trn = new O_vec2(...o_self.o_trn.a_n_comp)
+                        this.n_radius = 20
                         this.n_rot_rad = n_ang_rad
-                        this.n_corners = 3
-                        if((o_state.a_o_go__shot == undefined)){
-                            o_state.a_o_go__shot = []
-                        }
+                        // this.n_corners = 3
+
                         this.f_remove = function(){
                             o_state.a_o_go.splice(o_state.a_o_go.indexOf(this), 1)
                             o_state.a_o_go__shot.splice(o_state.a_o_go__shot.indexOf(this), 1)
@@ -221,13 +246,13 @@ let o_go__ship = new O_go(
                         if(this.n_count > 100){
                             this.f_remove();
                         }
-                        this.a_o_trn__shape_point = f_a_o_trn__shape_point__polygon(
-                            this.n_corners, 
-                            this.n_radius, 
-                            this.n_rot_rad,
-                            this.o_trn,
-                            this.s_color 
-                        )
+                        // this.a_o_trn__shape_point = f_a_o_trn__shape_point__polygon(
+                        //     this.n_corners, 
+                        //     this.n_radius, 
+                        //     this.n_rot_rad,
+                        //     this.o_trn,
+                        //     this.s_color 
+                        // )
                     },
                 )
             }
@@ -262,19 +287,34 @@ o_go__bad_meteroite = new O_go(
     function(){
         this.n_health = 100;
         this.n_rot_rad = 0;
-        this.o_trn = f_o_vec2(0)
+        this.o_trn = new O_vec2(0)
         let n_corners = parseInt(3+Math.random()*20);
+
+        this.o_vec2_min = new O_vec2(0);
+        this.o_vec2_max = new O_vec2(0);
 
         let n_rad_per_corner = (Math.PI*2)/n_corners;
         let n_radius_base = 50;
         this.a_o_trn__shape_point__original = []
         for(let n_c = 0; n_c < n_corners; n_c++){
             let n_rad = n_rad_per_corner *n_c;
+
+            const o_trn_new = new O_vec2(
+                (this.o_trn.n_x)+Math.sin(n_rad)*n_radius_base + Math.sin(Math.random()*n_tau)*n_radius_base*.2,
+                (this.o_trn.n_y)+Math.cos(n_rad)*n_radius_base + Math.sin(Math.random()*n_tau)*n_radius_base*.2
+            )
+            if(n_c == 0){
+                this.o_vec2_min = o_trn_new
+                this.o_vec2_max = o_trn_new
+            }else{
+                this.o_vec2_min.n_x = Math.min(this.o_vec2_min.n_x, o_trn_new.n_x)
+                this.o_vec2_min.n_y = Math.min(this.o_vec2_min.n_y, o_trn_new.n_y)
+                this.o_vec2_max.n_x = Math.max(this.o_vec2_max.n_x, o_trn_new.n_x)
+                this.o_vec2_max.n_y = Math.max(this.o_vec2_max.n_y, o_trn_new.n_y)
+            }
+
             this.a_o_trn__shape_point__original.push(
-                f_o_vec2(
-                    (this.o_trn.n_x)+Math.sin(n_rad)*n_radius_base + Math.sin(Math.random()*n_tau)*n_radius_base*.2,
-                    (this.o_trn.n_y)+Math.cos(n_rad)*n_radius_base + Math.sin(Math.random()*n_tau)*n_radius_base*.2
-                )
+                o_trn_new
             )
         }
         this.a_o_trn__shape_point = [...this.a_o_trn__shape_point__original]
@@ -282,7 +322,7 @@ o_go__bad_meteroite = new O_go(
     [],
     'rgba(255,0,255,0.8)',
     function(){
-        // let o_diff = f_o_vec2(o_state.o_go__ship).subtract(this.o_trn);
+        // let o_diff = new O_vec2(o_state.o_go__ship).subtract(this.o_trn);
         // let n_dist = o_diff.length();
 
         let n_ang_rad = this.o_trn.hangle(o_state.o_go__ship.o_trn)
@@ -300,19 +340,16 @@ o_go__bad_meteroite = new O_go(
 
         //check collision 
         for(let o_go__shot of o_state?.a_o_go__shot){
-            debugger
+            // debugger
             let b = f_b_collision__circle_and_polygonpoints(
-                {
-                    // we interpret a shot as a circle, sinec polygon overlapping detection would be 
-                    // to computation intesive
-                    radius: o_go__shot.n_radius,
-                    centerX: o_go__shot.o_trn.x,
-                    centerY: o_go__shot.o_trn.y
-                }, 
-                this.a_o_trn__shape_point
+                this.a_o_trn__shape_point, 
+                this.o_vec2_min,
+                this.o_vec2_max,
+                o_go__shot.n_radius, 
+                o_go__shot.o_trn
             )
-            console.log(b)
             if(b){
+                console.log(b)
                 this.n_health -=1;
                 o_go__shot.f_remove();
             }
@@ -331,9 +368,9 @@ o_state = {
         o_go__bad_meteroite
     ],
     a_o_go__shot: [],
-    o_trn__mouse : f_o_vec2(0),
-    o_trn__ship : f_o_vec2(0),
-    o_scl__ship: f_o_vec2(10,10),
+    o_trn__mouse : new O_vec2(0),
+    o_trn__ship : new O_vec2(0),
+    o_scl__ship: new O_vec2(10,10),
     n_t: 0,
     o_canvas: o_canvas,
     o_ctx: o_canvas.getContext('2d'),
@@ -366,6 +403,69 @@ let f_draw_shape = function(
     }
     o_ctx.fill();
 }
+class O_polygon_info{
+    constructor(
+        a_o_trn__shape_point,
+        a_o_normal,
+        o_vec2_min,
+        o_vec2_max,
+    ){
+        this.a_o_trn__shape_point = a_o_trn__shape_point
+        this.a_o_normal = a_o_normal
+        this.o_vec2_min = o_vec2_min
+        this.o_vec2_max = o_vec2_max
+    }
+}
+let f_o_polygon_info__regular = function(
+    n_corners, 
+    n_radius, 
+    n_rotation_rad,
+    o_trn,
+    s_color,
+    b_normals = true, 
+    b_rect_bounds = true
+){
+    let a_o_trn__shape_point = [];
+    let a_o_normal = [];
+    let a_o_trn__shape_rect_bounds = [];
+    let o_vec2_min = null;
+    let o_vec2_max = null;
+    let n_rad_per_corner = (Math.PI*2)/n_corners
+    for(let n_c = 0; n_c < n_corners;n_c+=1){
+        let n_rad = n_rad_per_corner*n_c-n_rotation_rad + (Math.PI*2)/4+Math.PI;
+        let o_trn = new O_vec2(
+            o_trn.n_x+Math.cos(n_rad)*n_radius,//+(n_radius),
+            o_trn.n_y+Math.sin(n_rad)*n_radius,//+(n_radius),
+        )
+        if(b_normals){
+            if(n_c > 0){
+                a_o_normal.push(o_trn.sub(a_o_trn__shape_point[n_c-1]))
+            }
+        }
+        if(b_rect_bounds){
+            if(n_c == 0){
+                o_vec2_min = o_trn
+                o_vec2_max = o_trn
+            }else{
+                o_vec2_min.n_x = Math.min(o_vec2_min.n_x, o_trn.n_x)
+                o_vec2_min.n_y = Math.min(o_vec2_min.n_y, o_trn.n_y)
+                o_vec2_max.n_x = Math.max(o_vec2_max.n_x, o_trn.n_x)
+                o_vec2_max.n_y = Math.max(o_vec2_max.n_y, o_trn.n_y)
+            }
+        }
+        a_o_trn__shape_point.push(
+            o_trn
+        )
+    }
+    return new O_polygon_info(
+        a_o_trn__shape_point, 
+        a_o_normal, 
+        o_vec2_min, 
+        o_vec2_max
+    )
+    return a_o_trn__shape_point
+}
+
 let f_a_o_trn__shape_point__polygon = function(
     n_corners, 
     n_radius, 
@@ -376,11 +476,11 @@ let f_a_o_trn__shape_point__polygon = function(
     let a_o_trn__shape_point = [];
     let n_rad_per_corner = (Math.PI*2)/n_corners
     for(let n_c = 0; n_c < n_corners;n_c+=1){
-        let n_rad = n_rad_per_corner*n_c-n_rotation_rad;
+        let n_rad = n_rad_per_corner*n_c-n_rotation_rad + (Math.PI*2)/4+Math.PI;
         a_o_trn__shape_point.push(
-            f_o_vec2(
-                o_trn.n_x+Math.sin(n_rad)*n_radius,//+(n_radius),
-                o_trn.n_y+Math.cos(n_rad)*n_radius,//+(n_radius),
+            new O_vec2(
+                o_trn.n_x+Math.cos(n_rad)*n_radius,//+(n_radius),
+                o_trn.n_y+Math.sin(n_rad)*n_radius,//+(n_radius),
             )
         )
     }
@@ -417,11 +517,41 @@ let f_render = function(o_ctx){
         for(let n_idx in o_state.a_o_go){
             let o_go = o_state.a_o_go[n_idx];
             // console.log(o_go.o_trn.toString())
-            f_draw_shape(
-                o_state.o_ctx,
-                o_go.a_o_trn__shape_point,
-                o_go.s_color
-            )
+            if(o_go.a_o_trn__shape_point){
+                f_draw_shape(
+                    o_state.o_ctx,
+                    o_go.a_o_trn__shape_point,
+                    o_go.s_color
+                )
+            }
+            if(
+                o_go.a_o_trn__shape_point?.length == 0 
+                 && o_go.n_radius
+                 && o_go.o_trn
+            ){
+                o_state.o_ctx.fillStyle = o_go.s_color
+                o_state.o_ctx.beginPath();
+                o_state.o_ctx.arc(
+                    o_go.o_trn.n_x,
+                    o_go.o_trn.n_y,
+                    o_go.n_radius,
+                    0, 
+                    2*Math.PI)
+                o_state.o_ctx.fill(); 
+
+                // draw bounding box
+                // let o_vec2_min_circle = o_go.o_trn.sub(o_go.n_radius);
+                // let o_vec2_max_circle = o_go.o_trn.add(o_go.n_radius);
+                // o_state.o_ctx.fillStyle = 'rgba(255,0,0,0.4)'
+                // o_state.o_ctx.rect(
+                //     o_vec2_min_circle.n_x,
+                //     o_vec2_min_circle.n_y, 
+                //     o_go.n_radius*2,
+                //     o_go.n_radius*2
+                // )
+                // o_state.o_ctx.fill(); 
+                
+            }
 
             if(o_go?.f_callback_on_render){
                 o_go?.f_callback_on_render(n_idx)
@@ -446,7 +576,7 @@ document.body.style.padding = '0';
 document.body.style.margin = '0';
 document.body.style.overflow = 'hidden';
 window.onmousemove = function(o_e){
-    o_state.o_trn__mouse = f_o_vec2(
+    o_state.o_trn__mouse = new O_vec2(
         o_e.clientX,
         o_e.clientY
     )
